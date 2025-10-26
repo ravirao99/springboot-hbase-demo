672ad9b2178850b1f212b4717114bde5e427ae7a
ravirao99 <73358764+ravirao99@users.noreply.github.com>
2025-10-26 19:00:16 +0000

    Add main Spring Boot application class

---
+++ src/main/java/com/example/hbase/SpringbootHbaseApplication.java
@@ -0,0 +1,11 @@
+package com.example.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHbaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHbaseApplication.class, args);
+    }
+}
